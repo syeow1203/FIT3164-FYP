8ddb0d675edf76308d4614d4e6f7935011cf7269
syeo0021 <syeo0021@student.monash.edu>
2024-05-11 14:30:14 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(),
+    package_data={'mypackage': ['data/*.csv']}, 
+    include_package_data=True,
+    description='A simple prediction package',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "joblib"
+    ],
+    
+    python_requires='>=3.6',
+    author='Yeow Shiau Yen',
+    author_email='syeo0021@student.monash.edu'
+)
