cdac3c04c8acfc0f898b0107305c535d1451ea40
syeo0021 <syeo0021@student.monash.edu>
2024-05-11 16:02:09 +0800

    update

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='mypackage',
     version='0.1',
     packages=find_packages(),
+        package_data={
+        'mypackage': ['data/model.pkl'],
+    },
+    include_package_data=True,
     description='A simple prediction package',
     long_description=open('README.md').read(),
     install_requires=[
